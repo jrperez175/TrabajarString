e9c82f9f02e14ab615441666cc83f3154945b6dc
Jhon Perez <jrperez175@gmail.com>
2018-02-23 11:18:29 -0500

    Commit 23Feb2018

---
+++ test/TrabajarConString/encontrarCadenaTest.java
@@ -0,0 +1,29 @@
+package TrabajarConString;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class encontrarCadenaTest {
+
+	@Test
+	public void testBuscarStopCodon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEncontrarGen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAlmacenarTodosGenes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
